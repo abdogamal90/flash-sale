b55510a1b6255d0e21e55b3da87fc880636bc6ce
Abdelrahman Gamal <62604408+abdogamal90@users.noreply.github.com>
2025-11-28 16:41:51 +0200

    intial commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\Product;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+
+class DatabaseSeeder extends Seeder
+{
+    use WithoutModelEvents;
+
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        Product::factory()->create([
+            'name' => 'Sample Product',
+            'available_stock' => 100,
+            'price' => 29.99,
+        ]);
+    }
+}
